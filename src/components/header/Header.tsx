f852b3e856e4c2e71bca823cb636a5e87c3aa7f3
Veronica <vveronikanika@gmail.com>
2023-08-21 16:57:43 +0300

    fix: change style for header, create config for tailwind theme

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React, {useState} from 'react';
+
+import { Karantina } from 'next/font/google';
+
+const karantina = Karantina({   weight: ['400', '700'],
+style: ['normal'],
+subsets: ['latin'],
+display: 'swap'})
+
 const Header = () => {
 
     const [nav, setNav] = useState(false);
@@ -12,49 +20,57 @@ const Header = () => {
     }
 
   return (
-    <div className="cont mt-5">
-      {/* <Link href='/'>LOGO</Link> */}
-      <nav className="max-w-[1440px] m-auto flex justify-between items-center py-5 text-white">
-        <Link href="/">LOGO</Link>
-        <ul className="hidden sm:flex sm:gap-6 xl:gap-14">
-          <li>
-            <Link className='py-3 xl:py-7' href="/about">About</Link>
+    <div className="px-5 md:px-8 lx:px-28 mt-5">
+      <nav className="max-w-[1440px] m-auto flex justify-between items-center align-baseline py-5 text-white">
+        <Link href="/">
+        <Image
+          src='/logo.png'
+          alt='logo'
+          width={59}
+          height={21}
+          className='object-contain'
+        />
+        <span className={`${karantina.className} text-sm tracking-[2.59px]`}>CarpTravel</span>
+        </Link>
+        <ul className="hidden sm:flex sm:gap-6 xl:gap-14 text-sm tracking-[1.4px]">
+          <li onClick={handleNav}>
+            <Link className='py-3 xl:py-7 hover:underline underline-offset-4'  href="/about">About</Link>
           </li>
-          <li>
-            <Link className='py-3 xl:py-7' href="/services">Services</Link>
+          <li onClick={handleNav}>
+            <Link className='py-3 xl:py-7 hover:underline underline-offset-4' href="/services">Services</Link>
           </li>
-          <li>
-            <Link className='py-3 xl:py-7' href="/career">Career</Link>
+          <li onClick={handleNav}>
+            <Link className='py-3 xl:py-7  hover:underline underline-offset-4' href="/career">Career</Link>
           </li>
-          <li>
-            <Link className='py-3 xl:py-7' href="/gallery">Gallery</Link>
+          <li onClick={handleNav}>
+            <Link className='py-3 xl:py-7  hover:underline underline-offset-4' href="/gallery">Gallery</Link>
           </li>
-          <li>
-            <Link className='py-3 xl:py-7' href="/contacts">Contacts</Link>
+          <li onClick={handleNav}>
+            <Link className='py-3 xl:py-7  hover:underline underline-offset-4' href="/contacts">Contacts</Link>
           </li>
         </ul>
         {/* Mobile button*/}
         <div onClick={handleNav} className="block sm:hidden z-10">
-            <button className='uppercase mr-0'>{nav ?  "close": 'Menu'}</button>
+            <button className='uppercase mr-0 text-sm tracking-[1.4px]'>{nav ?  "close": 'Menu'}</button>
         </div>
         
         {/* Mobile menu */}
         {/* <div className=' '> */}
-            {/* <button >close</button> */}
-        <ul className={nav ? "sm:hidden absolute top-0 left-0 bottom-0 right-0 w-full h-screen backdrop-blur-xl flex flex-col justify-center items-center  text-center ease-in duration-300" : "sm:hidden absolute top-0 left-[-100%] bottom-0 right-0 w-full h-screen backdrop-blur-xl flex flex-col justify-center items-center  text-center ease-in duration-300"}>
-        <li className='hover:underline-offset-4'>
+            {/* <button >close</button> */} 
+        <ul className={nav ? "sm:hidden absolute top-0 left-0 bottom-0 right-0 w-full h-screen backdrop-blur-xl flex flex-col justify-center items-center tracking-[1.8px]  text-center ease-in duration-300" : "sm:hidden absolute top-0 left-[-100%] bottom-0 right-0 w-full h-screen backdrop-blur-xl flex flex-col justify-center items-center  text-center ease-in duration-300"}>
+            <li onClick={handleNav}>
               <Link href="/about" className="p-6 text-lg block">About</Link>
             </li>
-            <li onClick={handleNav} className='hover:underline-offset-4'>
+            <li onClick={handleNav}>
               <Link href="/services" className="p-6 text-lg block">Services</Link>
             </li>
-            <li  onClick={handleNav} className='hover:underline-offset-4'>
+            <li  onClick={handleNav}>
               <Link href="/career" className="p-6 text-lg block">Career</Link>
-            </li>
-            <li onClick={handleNav} className='hover:underline-offset-4'>
+            </li>              
+            <li onClick={handleNav}>
               <Link href="/gallery" className="p-6 text-lg block">Gallery</Link>
             </li>
-            <li onClick={handleNav} className='hover:underline-offset-4'>
+            <li onClick={handleNav}>
               <Link href="/contacts" className="p-6 text-lg block">Contacts</Link>
             </li>
         </ul>
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,7 @@
 import {FC} from 'react';
 import {Hero, Header} from '..';
 
+
 const Home: FC = () => {
   return (     
     <>
