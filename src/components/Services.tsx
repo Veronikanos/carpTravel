c7654b28dc212bc42a02adf3bf0b7704012f4783
Veronica <vveronikanika@gmail.com>
2023-08-24 00:57:14 +0300

    feat(swiper): implement simple slider

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,13 +1,23 @@
-'use client'                                                                              
+'use client'
 
 import Image from 'next/image';
-import {FC, useEffect, useState} from 'react';
+import { FC, useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { ContentItem } from './types';
+import { Controller, Navigation, Pagination, Scrollbar, A11y,  EffectFade } from 'swiper/modules';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import Slider from './Slider';
 
+
 const Services: FC = () => {
-
-    const [data, setData] = useState([]);
-
+    const [data, setData] = useState<ContentItem[]>([]);
+    const [controlledSwiper, setControlledSwiper] = useState<any>(null);
+    const [activeItem, setActiveItem] = useState<number>(0); // Initialize activeItem state
+ 
     useEffect(() => {
       // Fetch data from content.json 
       fetch('/content.json')
@@ -17,36 +27,91 @@ const Services: FC = () => {
     }, []);
 
 
-  return (
-    <section>
-      <div className="padding-x services-section">
-        <div className="padding-y ">
-          <h2 className="header-2 mb-[24px] md:mb-0">
-            We <span className="font-medium">offer</span>
-          </h2>
-          <div className="header-2 ml-auto">
-            01/<span className="text-light-transparent">05</span>
+  // Access activeIndex from controlledSwiper
+  const activeIndex = controlledSwiper?.activeIndex ?? 0;
+
+  // Update the activeItem state with a new value
+  const handleActiveItemChange = (newActiveItem: number) => {
+    setActiveItem(newActiveItem);
+  };
+    return (
+      <section>
+        <div className="padding-x services-section">
+          <div className="padding-y">
+            <h2 className="header-2 mb-[24px] md:mb-0">
+              We <span className="font-medium">offer</span>
+            </h2>
+            <div className="header-2 ml-auto">01/0{data.length}
+            </div>
+  
+            <div className="my-[16px] max-w-[463px] md:max-w-[463px] xl:max-w-[607px]">
+
+              
+              
+              <Swiper 
+              modules={[Controller, Navigation, Pagination, Scrollbar, A11y]}
+            //   modules={[EffectFade]} effect="fade"
+                spaceBetween={50}
+                slidesPerView={1}
+                pagination={{clickable: true}}
+                
+                controller={{ control: controlledSwiper }}
+    //   onSlideChange={(swiper) => handleActiveItemChange(swiper.activeIndex)}
+      onSlideChange={(swiper) => console.log(swiper)}
+              >
+                {data.map((item, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="">
+                      <Image
+                        src={item.image}
+                        width={607}
+                        height={429}
+                        sizes="(max-width: 320px) 280px, (max-width: 768px) 463px, (max-width: 1280px) 607px"
+                        alt={item.alt}
+                        layout="responsive"
+                      />
+                    </div>
+                    <p className="mb-[24px]">{item.title}</p>
+                    <Slider/>
+            <p>
+              {item.description}
+            </p>
+   
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+
+              <Swiper
+        modules={[Controller]}
+        onSwiper={setControlledSwiper}
+      >
+             <SwiperSlide> {activeIndex}</SwiperSlide> 
+             <SwiperSlide> {activeIndex}</SwiperSlide> 
+             <SwiperSlide> {activeIndex}</SwiperSlide> 
+             <SwiperSlide> {activeIndex}</SwiperSlide> 
+             <SwiperSlide> {activeIndex}</SwiperSlide> 
+      </Swiper>
+
+
+            </div>
+  
+            {/* <ul className="mb-[34px] services-list flex flex-col gap-[16px]">
+              {data.map((item, index) => (
+                <li
+                  key={index}
+                  className={index === activeSlide ? 'services-item__active' : ''}
+                  onClick={() => handleListItemClick(index)}
+                >
+                  {item.title}
+                </li>
+              ))}
+            </ul> */}
+  
+
           </div>
-
-          <Slider data={data}/>
-
-          <ul className="mb-[34px] services-list flex flex-col gap-[16px]">
-            <li className="services-item__active">ATVs Traveling</li>
-            <li>Rock climbing</li>
-            <li>Hot air ballooning</li>
-            <li>Skydiving</li>
-            <li>Rafting</li>
-          </ul>
-          <p>
-            Join exciting rafting expeditions on the waterways of the
-            Carpathians. Go through challenging waterways and overcome
-            gusty waves, feel the adrenaline, and enjoy the incredible
-            views of the surrounding mountains.
-          </p>
         </div>
-      </div>
-    </section>
-  );
-};
-
-export default Services;
+      </section>
+    );
+  };
+  
+  export default Services;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,30 +1,14 @@
 import Image from 'next/image';
-import {ContentItem} from './types';
+import { FC } from 'react';
 
 
-interface SliderProps {
-  data: ContentItem[];
-}
 
-const Slider: React.FC<SliderProps> = ({data}) => {
+
+const Slider: FC = () => {
+  
+
   return (
-    <>
-      {data.map((item, index) => (
-        <div key={index} className="">
-          <div className="my-[16px] max-w-[463px] md:max-w-[463px] xl:max-w-[607px]">
-            <Image 
-              src={item.image}
-              width={607}
-              height={429}
-              sizes="(max-width: 320px) 280px, (max-width: 768px) 463px, (max-width: 1280px) 607px"
-              alt={item.alt}
-              layout="responsive"
-            />
-          </div>
-          <p className="mb-[24px]">{item.title}</p>
-        </div>
-      ))}
-    </>
+    <h2>ghghghg</h2>
   );
 };
 
