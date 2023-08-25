0a6250b69bc83464994c16764b8beb2f92ffabd3
Veronica <vveronikanika@gmail.com>
2023-08-26 01:01:27 +0300

    fix config again, success run dev locally

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-  }
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
+}
+
+module.exports = nextConfig
