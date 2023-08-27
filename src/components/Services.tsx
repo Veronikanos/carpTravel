8bbaddae80da80e8455accc0ff80492be6d3c6c9
Veronica <vveronikanika@gmail.com>
2023-08-27 18:45:21 +0300

    fix: handle position of slider inside another slider

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -15,46 +15,45 @@ const Services: FC = () => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [activeBg, setActiveBg] = useState<string>(slides[0].bg);
 
-//   const onClickBtn = (index: number) => {
+  //   const onClickBtn = (index: number) => {
 
-//     swiperInstance.slideTo(index);
-//   };
+  //     swiperInstance.slideTo(index);
+  //   };
 
   const [controlledSwiper, setControlledSwiper] = useState<any>(null);
   return (
     <>
       <section id="services">
-        <div className="services-section  max-width">
+        {/* <h2>We offer</h2> */}
+        <div className="services-section  max-width relative ">
           <Swiper
             modules={[Controller, EffectFade, Navigation]}
             controller={{control: controlledSwiper}}
             slidesPerView={1}
-                onSlideChange={() => console.log(controlledSwiper)}
-                >
+            // noSwiping={true}
+            onSlideChange={() => console.log(controlledSwiper)}>
             {slides.map((item) => {
               return (
                 <SwiperSlide key={item.id}>
-                  <div className="w-full flex flex-col items-center justify-center relative">
+                  <div className="w-full h-[851px] md:h-[621px] xl:h-[779px]   relative">
                     {/* The next/image component */}
-                    <div className="absolute inset-0 z-[-1]">
+                    {/* <div className="absolute inset-0 z-[-1]"> */}
                       <Image
                         src={item.bg}
                         alt={item.title}
                         layout="fill"
                         objectFit="cover"
-                        priority
-                        loading="eager"
                         className="object-cover"
                       />
-                    </div>
+                    {/* </div> */}
 
                     {/* Content inside the slide */}
-                    <div className="relative z-10 text-center p-8 bg-black bg-opacity-50 text-white">
+                    {/* <div className="relative text-center p-8 bg-black bg-opacity-50 text-white">
                       <h2 className="text-2xl font-semibold mb-4">
                         {item.title}
                       </h2>
                       <p className="text-base">{item.description}</p>
-                    </div>
+                    </div> */}
                   </div>
                 </SwiperSlide>
               );
@@ -62,29 +61,29 @@ const Services: FC = () => {
           </Swiper>
 
           {/* Controlled Swiper -> store swiper instance */}
-          <div className="w-[200px] h-[400px] relative">
-            <Swiper
-              modules={[Controller]}
-              onSwiper={setControlledSwiper}>
-                  {slides.map((item) => {
-              return (<SwiperSlide key={item.title}>
-                      <div className="w-full h-full relative">
-                        <Image
-                          src={item.picture}
-                          alt="Nested Slide Image"
-                          width={307}
-                          height={229}
-                          objectFit="cover"
-                          priority
-                          loading="eager"
-                          className="object-cover"
-                        />
-                      </div>
-                    </SwiperSlide>
-                    )
-                  })}
-            </Swiper>
-          </div>
+          <div className="absolute inset-0 padding-x">
+            {/* text-block */}
+        <div className="relative">
+            <span className="header-2">We offer</span>
+            </div>
+
+
+          <Swiper modules={[Controller]} onSwiper={setControlledSwiper} className='w-full h-[213px] sm:h-[370px] xl:h-[429px] sm:w-[463px] md:w-[463px] lg:w-[607px]' 
+
+          >
+            {slides.map((item) => (
+              <SwiperSlide key={item.title} >
+                    <Image
+                      src={item.picture}
+                      alt="Nested Slide Image"
+                      layout="fill"
+                      objectFit="cover"
+                      className="object-cover"
+                    />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
         </div>
       </section>
     </>
