0bbc4831f378efed9875e8b9ef9a1e1843186b70
veronikanos <vveronikanika@gmail.com>
2023-09-01 17:48:43 +0300

    add ogp data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,26 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'CarpTravel',
   description: 'unforgettable trips to the most beautiful parts of the Carpathians',
+  viewport: 'width=device-width, initial-scale=1.0',
+  robots: 'index, follow',
+  openGraph: {
+    type: 'website',
+    url: 'https://carp-travel-nu.vercel.app/',
+    title: 'CarpTravel',
+    description: 'Uncover secrets of the Carpatian mountains',
+    siteName: 'CarpTravel',
+    images: [{url: '/logo.png'}],
+  },
+  applicationName: 'CarpTravel',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Travel', 'Tourism', 'Adrenaline'],
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+    category: 'tourism',
 }
 
 export default function RootLayout({
